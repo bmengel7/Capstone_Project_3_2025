fa883d3ee68f87a0cb8f9d0296a6a4d3c5121da6
bmengel7 <brie.mengel@yahoo.com>
2025-03-23 14:54:49 -0400

    adding paths for joins

--- sk_load.py
+++ sk_load.py
@@ -5,7 +5,10 @@ import pandas as pd
 import duckdb
 
 def main():
- 
+
+data_dir = 'data'
+duckdb_path = os.path.join(data_dir, 'serial_killers.duckdb')
+output_csv_path = os.path.join(data_dir, 'output.csv')
  
 if __name__ == "__main__":  
     main()
